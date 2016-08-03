ece2e1cc9791ec49098ff4c63ae388ab921cf6a5
Babatunde Adeyemi <barbietunnie@gmail.com>
2016-08-03 18:48:35 +0100

    Save image to camera roll on double click

--- index.ios.js
+++ index.ios.js
@@ -1,67 +1,55 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * @flow
  */
-'use strict';
-var ShakeEvent = require('react-native-shake-event-ios');
-var NetworkImage = require('react-native-image-progress');
-var Progress = require('react-native-progress');
-var Swiper = require('react-native-swiper');
-var RandManager = require('./RandManager.js');
-var Utils = require('./Utils.js');
-var React = require('react-native');
 
-// Components 
-var ProgressHUD = require('./ProgressHUD.js');
-
-var {
+import React, { Component } from 'react';
+import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Component,
-  ActivityIndicatorIOS,
+  ActivityIndicator,
   Dimensions,
   PanResponder,
   CameraRoll,
   AlertIOS
-} = React;
+} from 'react-native';
+
+var Swiper = require('react-native-swiper');
+var NetworkImage = require('react-native-image-progress');
+var Progress = require('react-native-progress');
+var RandManager = require('./utils/RandManager');
+var Utils = require('./utils/Utils');
+
+const NUM_WALLPAPERS = 5;
+const DOUBLE_TAP_DELAY = 300; // ms
+const DOUBLE_TAP_RADIUS = 20;
 
 var {width, height} = Dimensions.get('window');
 
-const NUM_WALLPAPERS = 5;
-const DOUBLE_TAP_DELAY = 300; // milliseconds
-const DOUBLE_TAP_RADIUS = 20;
-
-class SplashWalls extends Component{
+class SplashWalls extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       wallsJSON: [],
-      isLoading: true,
-      isHudVisible: false
+      isLoading: true
     };
 
     this.imagePanResponder = {};
 
-    // Previous touch information
     this.prevTouchInfo = {
       prevTouchX: 0,
       prevTouchY: 0,
-      prevTouchTimeStamp: 0
+      prevTouchTimestamp: 0
     };
 
-    this.currentWallIndex = 0;
-
-    // Binding this to methods
     this.handlePanResponderGrant = this.handlePanResponderGrant.bind(this);
     this.onMomentumScrollEnd = this.onMomentumScrollEnd.bind(this);
 
-  }
-
-  componentDidMount() {
-    this.fetchWallsJSON();      
+    this.currentWallIndex = 0;
   }
 
   componentWillMount() {
@@ -71,37 +59,74 @@ class SplashWalls extends Component{
       onPanResponderRelease: this.handlePanResponderEnd,
       onPanResponderTerminate: this.handlePanResponderEnd
     });
-
-    // Fetch new wallpapers on shake
-    ShakeEvent.addEventListener('shake', () => {
-      this.initialize();
-      this.fetchWallsJSON();
-    });
   }
 
-  render() {
-    var {isLoading} = this.state;
-    if(isLoading)
-      return this.renderLoadingMessage();
-    else
-      return this.renderResults();
+  handleStartShouldSetPanResponder(e, gestureState) {
+    return true;
   }
 
-  initialize() {
-    this.setState({
-      wallsJSON: [],
-      isLoading: true,
-      isHudVisible: false
-    });
+  handlePanResponderGrant(e, gestureState) {
+    console.log('Finger touched the Image!');
 
-    this.currentWallIndex = 0;
+    var currentTouchTimestamp = Date.now();
+
+    if( this.isDoubleTap(currentTouchTimestamp, gestureState) ) {
+      console.log('Double tap detected!');
+
+      this.saveCurrentWallpaperToCameraRoll();
+    }
+
+    this.prevTouchInfo = {
+      prevTouchX: gestureState.x0,
+      prevTouchY: gestureState.y0,
+      prevTouchTimestamp: currentTouchTimestamp
+    }
+  }
+
+  handlePanResponderEnd(e, gestureState) {
+    console.log('Finger pulled up from Image!');
+  }
+
+  onMomentumScrollEnd(e, state, context) {
+    this.currentWallIndex = state.index;
+  }
+
+  isDoubleTap(currentTouchTimestamp, {x0, y0}) {
+    var {prevTouchX, prevTouchY, prevTouchTimestamp} = this.prevTouchInfo;
+    var dt = currentTouchTimestamp - prevTouchTimestamp;
+
+    return (dt < DOUBLE_TAP_DELAY && Utils.distance(prevTouchX, prevTouchY, x0, y0) < DOUBLE_TAP_RADIUS);
+  }
+
+  saveCurrentWallpaperToCameraRoll() {
+    var {wallsJSON} = this.state;
+
+    var currentWall = wallsJSON[this.currentWallIndex];
+    var currentWallURL = `https://unsplash.it/${currentWall.width}/${currentWall.height}?image=${currentWall.id}`;
+
+    CameraRoll.saveToCameraRoll(currentWallURL, 'photo')
+      .then(
+        (data) => {
+          AlertIOS.alert(
+            'Saved',
+            'Wallpaper successfully saved to Camera Roll',
+            [
+              {text: 'High 5!', onPress: () => console.log('Ok Pressed!')}
+            ]
+          );
+        })
+        .catch(
+          (err) => {
+            console.log('Error saving to camera roll', err);
+          }
+        );
   }
 
   fetchWallsJSON() {
-    var url = 'http://unsplash.it/list';
+    var url = 'https://unsplash.it/list';
     fetch(url)
-      .then( response => response.json() )
-      .then( jsonData => {
+      .then(response => response.json())
+      .then(jsonData => {
         var randomIds = RandManager.uniqueRandomNumbers(NUM_WALLPAPERS, 0, jsonData.length);
         var walls = [];
         randomIds.forEach(randomId => {
@@ -113,124 +138,108 @@ class SplashWalls extends Component{
           wallsJSON: [].concat(walls)
         });
       })
-      .catch( error => console.log('JSON Fetch error : ' + error) );
+      .catch(error => console.log('JSON Fetch Error: ' + error));
+  }
+
+  componentDidMount() {
+    this.fetchWallsJSON();
   }
 
   renderLoadingMessage() {
     return (
       <View style={styles.loadingContainer}>
-        <ActivityIndicatorIOS
+        <ActivityIndicator
           animating={true}
-          color={'#fff'}
-          size={'small'} 
+          color={'#FFF'}
+          size={'small'}
           style={{margin: 15}} />
-          <Text style={{color: '#fff'}}>Contacting Unsplash</Text>
+
+        <Text style={{color: '#FFF'}}> Contacting Unsplash </Text>
       </View>
     );
-  }
-
-  saveCurrentWallpaperToCameraRoll() {
-    // Make Progress HUD visible
-    this.setState({isHudVisible: true});
-
-    var {wallsJSON} = this.state;
-    var currentWall = wallsJSON[this.currentWallIndex];
-    var currentWallURL = `http://unsplash.it/${currentWall.width}/${currentWall.height}?image=${currentWall.id}`;
-
-    CameraRoll.saveImageWithTag(currentWallURL, (data) => {
-      // Hide Progress HUD
-      this.setState({isHudVisible: false});
-
-      AlertIOS.alert(
-        'Saved',
-        'Wallpaper successfully saved to Camera Roll',
-        [
-          {text: 'High 5!', onPress: () => console.log('OK Pressed!')}
-        ]
-      );
-    },(err) =>{
-      console.log('Error saving to camera roll', err);
-    });
-  }
-
-  onMomentumScrollEnd(e, state, context) {
-    this.currentWallIndex = state.index;
   }
 
   renderResults() {
-    var {wallsJSON, isHudVisible} = this.state;
+    var {wallsJSON, isLoading} = this.state;
+    if ( !isLoading ) {
+      return (
+        <Swiper
+         dot={<View style={{backgroundColor: 'rgba(255, 255, 255, .4)', width: 8, height: 8, borderRadius: 10, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
+         activeDot={<View style={{backgroundColor: '#FFF', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
+         loop={true}
+         onMomentumScrollEnd={this.onMomentumScrollEnd}>
+          {
+            wallsJSON.map((wallpaper, index) => {
+              return (
+                <View key={index}>
+                  <NetworkImage
+                    source={{uri: `https://unsplash.it/${wallpaper.width}/${wallpaper.height}?image=${wallpaper.id}`}}
+                    indicator={Progress.Circle}
+                    style={styles.wallpaperImage}
+                    indicatorProps={
+                      {
+                        color: 'rgba(255, 255, 255)',
+                        size: 60,
+                        thickness: 7
+                      }
+                    }
+                    {...this.imagePanResponder.panHandlers}>
+                      <Text style={styles.label}>Photo by</Text>
+                      <Text style={styles.labelAuthorName}>{wallpaper.author}</Text>
+                  </NetworkImage>
+                </View>
+              );
+            })
+          }
+        </Swiper>
+      );
+    }
+  }
+
+  render() {
+    var {isLoading} = this.state;
+    if(isLoading)
+      return this.renderLoadingMessage();
+    else
+      return this.renderResults();
+
     return (
-      <View>
-      <Swiper
-        dot={<View style={{backgroundColor:'rgba(255,255,255,.4)', width: 8, height: 8,borderRadius: 10, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />}
-        activeDot={<View style={{backgroundColor: '#fff', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
-        onMomentumScrollEnd={this.onMomentumScrollEnd}
-        index={this.currentWallIndex}
-        loop={false}>
-        {wallsJSON.map((wallpaper, index) => {
-          return(
-            <View key={wallpaper.id}>
-              <NetworkImage
-                source={{uri: `https://unsplash.it/${wallpaper.width}/${wallpaper.height}?image=${wallpaper.id}`}}
-                indicator={Progress.Circle}
-                indicatorProps={{
-                  color: 'rgba(255, 255, 255)',
-                  size: 60,
-                  thickness: 7
-                }}
-                threshold={0}
-                style={styles.wallpaperImage}
-                {...this.imagePanResponder.panHandlers}>
-
-                  <Text style={styles.label}>Photo by</Text>
-                  <Text style={styles.label_authorName}>{wallpaper.author}</Text>
-
-              </NetworkImage>
-            </View>
-          );
-        })}
-      </Swiper>
-      <ProgressHUD width={width} height={height} isVisible={isHudVisible}/>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
       </View>
     );
   }
+}
 
-  // Pan Handler methods
-  handleStartShouldSetPanResponder(e, gestureState){
-    return true;
-  }
-
-  handlePanResponderGrant(e, gestureState) {
-    var currentTouchTimeStamp = Date.now();
-
-    if( this.isDoubleTap(currentTouchTimeStamp, gestureState) ) 
-      this.saveCurrentWallpaperToCameraRoll();
-
-    this.prevTouchInfo = {
-      prevTouchX: gestureState.x0,
-      prevTouchY: gestureState.y0,
-      prevTouchTimeStamp: currentTouchTimeStamp
-    };
-  }
-
-  handlePanResponderEnd(e, gestureState) {
-    // When finger is pulled up from the screen
-  }
-
-  isDoubleTap(currentTouchTimeStamp, {x0, y0}) {
-    var {prevTouchX, prevTouchY, prevTouchTimeStamp} = this.prevTouchInfo;
-    var dt = currentTouchTimeStamp - prevTouchTimeStamp;
-
-    return (dt < DOUBLE_TAP_DELAY && Utils.distance(prevTouchX, prevTouchY, x0, y0) < DOUBLE_TAP_RADIUS);
-  }
-
-
-};
-
-var styles = StyleSheet.create({
-  loadingContainer: {
-    flexDirection: 'row',
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  loadingContainer: {
+    flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#000'
@@ -243,7 +252,7 @@ var styles = StyleSheet.create({
   },
   label: {
     position: 'absolute',
-    color: '#fff',
+    color: '#FFF',
     fontSize: 13,
     backgroundColor: 'rgba(0, 0, 0, 0.8)',
     padding: 2,
@@ -252,9 +261,9 @@ var styles = StyleSheet.create({
     left: 20,
     width: width/2
   },
-  label_authorName: {
+  labelAuthorName: {
     position: 'absolute',
-    color: '#fff',
+    color: '#FFF',
     fontSize: 15,
     backgroundColor: 'rgba(0, 0, 0, 0.8)',
     padding: 2,
